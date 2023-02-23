7bd12815432cff83f72ba639da2601cb24c2ec6d
bachtiar <bachtiar@privyid.tech>
2023-02-24 04:03:16 +0700

    fixing dataConfig struct

--- publish_log.go
+++ publish_log.go
@@ -4,16 +4,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 
 	"cloud.google.com/go/pubsub"
 	"google.golang.org/api/option"
 )
 
 type dataConfig struct {
-	projectID string
-	topicID	  string
-	credentialFileJson []byte
+	ProjectID string
+	TopicID	  string
+	CredentialFileJson []byte
 }
 
 func PublishLog(ctx context.Context, data map[string]interface{}, config []byte) error {
@@ -23,20 +22,19 @@ func PublishLog(ctx context.Context, data map[string]interface{}, config []byte)
 
 	d, err := json.Marshal(data)
 	if err != nil {
-		log.Fatal("error when marshal data :", err)
+		fmt.Printf("error marshal :", err)
 	}
 
-	client, errClient := pubsub.NewClient(ctx, cfg.projectID, option.WithCredentialsJSON(cfg.credentialFileJson))
+	client, err := pubsub.NewClient(ctx, cfg.ProjectID, option.WithCredentialsJSON(cfg.CredentialFileJson))
 	if err != nil {
-		return fmt.Errorf("logger pubsub: NewClient: %v", errClient)
+		return fmt.Errorf("pubsub: NewClient: %v", err)
 	}
 	defer client.Close()
 
-	t := client.Topic(cfg.topicID)
+	t := client.Topic(cfg.TopicID)
 	result := t.Publish(ctx, &pubsub.Message{
 		Data: []byte(d),
 	})
-	
 	id, err := result.Get(ctx)
 	if err != nil {
 		return fmt.Errorf("pubsub: result.Get: %v", err)
